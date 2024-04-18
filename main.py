be52c1ca1b6dc0af18ab92a71a5fc09b7cea1402
5pponent <ahdwjdtprtm@gmail.com>
2024-04-18 14:11:35 +0900

    docs: Add app description

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse
 
 
+app_description = """
+### Plan with me API 문서입니다.
+
+**요구 사항**
+- 데이터를 포함하여 요청 시 **Content-Type** 헤더의 값을 **application/json** 으로 하여 요청합니다.
+- 인증이 필요한 API는 **Authorization** 헤더의 값을 **<auth_type> <access_token>** 으로 하여 요청합니다.
+- 문서에서 API 테스트 시 인증이 필요한 API는 우측의 **Authorize** 버튼을 누르고 액세스 토큰 값을 입력한 후 테스트합니다.
+"""
 app = FastAPI(
-    title="Plan with me",
+    title="Plan with me API",
     root_path="/api",
     docs_url='/docs',
     redoc_url='/redoc',
+    contact={
+        "email": "iee785@dongyang.ac.kr",
+    },
+
+    description=app_description,
 )
 
 import core.config.app_config
