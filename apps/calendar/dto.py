6af6917f3fb7ff59cfbc4af79f2445715b749117
5pponent <ahdwjdtprtm@gmail.com>
2024-06-03 15:35:34 +0900

    fix: Revive get specific calendar info api

--- apps/calendar/dto.py
+++ apps/calendar/dto.py
@@ -2,15 +2,17 @@ from typing import Any
 from pydantic import BaseModel
 
 from core.base_dto import BaseResponse
-from apps.user import dto as user_dto
+# from apps.user import dto as user_dto
 
 
-class CalendarSimpleResponse(BaseResponse):
+class CalendarResponse(BaseResponse):
     name: str
+    introduction: str | None = None
     image: str | None
 
 
 class CalendarForm(BaseModel):
     name: str
+    introduction: str | None = None
     image: str | None = None
     # user_ids: list[int] = []
